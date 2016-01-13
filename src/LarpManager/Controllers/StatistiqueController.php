afee7096589955f51331c9ecff361aff7d924d3e
KevinPolez <kevinpolez@gmail.com>
2016-01-13 16:03:07 +0100

    Mise à jour de la page des statistiques.

--- src/LarpManager/Controllers/StatistiqueController.php
+++ src/LarpManager/Controllers/StatistiqueController.php
@@ -52,9 +52,50 @@ class StatistiqueController
 					'label' => $classe->getLabel(),
 			);
 		}
+		
+		$repo = $app['orm.em']->getRepository('LarpManager\Entities\Personnage');
+		$personnages = $repo->findAll();
+		
+		$repo = $app['orm.em']->getRepository('LarpManager\Entities\User');
+		$users = $repo->findAll();
+		
+		$repo = $app['orm.em']->getRepository('LarpManager\Entities\Participant');
+		$participants = $repo->findAll();
+		
+		$repo = $app['orm.em']->getRepository('LarpManager\Entities\Groupe');
+		$groupes = $repo->findAll();
+		$places = 0;
+		foreach( $groupes as $groupe)
+		{
+			$places += $groupe->getClasseOpen();	
+		}
+		
+		$repo = $app['orm.em']->getRepository('LarpManager\Entities\Genre');
+		$genres = $repo->findAll();
+		$statGenres = array();
+		
+		foreach( $genres as $genre)
+		{
+			$colors = RandomColor::many(2, array(
+					'luminosity' => array('light', 'bright'),
+					'hue' => 'random'
+			));
+			$statGenres[] = array(
+					'value' => $genre->getPersonnages()->count(),
+					'color' => $colors[0],
+					'highlight' => $colors[1],
+					'label' => $genre->getLabel(),
+			);
+		}
+		
 		return $app['twig']->render('admin/statistique/index.twig', array(
 				'langues' => json_encode($stats),
 				'classes' => json_encode($statClasses),
+				'genres' => json_encode($statGenres),
+				'personnageCount' => count($personnages),
+				'userCount' => count($users),
+				'participantCount' => count($participants), 
+				'places' => $places,
 		));
 	}
 }
